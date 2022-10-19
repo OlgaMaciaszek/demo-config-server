df7f2c39ce963a615a8bf8fcbb3a6401129dcc2b
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-10-19 15:12:26 +0200

    Init repo.

---
+++ src/test/java/com/example/democonfigserver/DemoConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.democonfigserver;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
